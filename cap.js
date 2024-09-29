0ad03cdb6fd99a3b19d06c74f9d27b18110179ac
Deepak Singh <54103399+dksr789@users.noreply.github.com>
2024-09-29 17:25:46 +0530

    Update cap.js
    ppp

--- cap.js
+++ cap.js
@@ -2,10 +2,12 @@ const express = require('express');
 const axios = require('axios');
 const bodyParser = require('body-parser');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors()); // Enable CORS
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
